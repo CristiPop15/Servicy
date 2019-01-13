16f4fd767c44e221afb61666db6aa1a596ed5b55
Pop, Cristian (Accesa) <popc@mediamarktsaturn.com>
2019-01-13 20:36:45 +0200

    Hello World

---
+++ servicy/src/main/java/com/servicy/servicy/ServicyApplication.java
@@ -0,0 +1,14 @@
+package com.servicy.servicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicyApplication.class, args);
+	}
+
+}
+
